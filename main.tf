ebc31a01bc81926b2c1b55cc1b19a82ae2dc3bcf
nemar23 <66174534+nemar23@users.noreply.github.com>
2022-09-11 18:40:08 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,23 +35,6 @@ resource "aws_s3_bucket_acl" "b-acl" {
   acl = "public-read"
 }
 
-# POLICY S3
-resource "aws_s3_bucket_policy" "b-policy" {
-  bucket = aws_s3_bucket.b.id
-
-  policy      = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "arn:aws:s3:::s3-95128/*",
-      }
-    ]
-	})
-}
-
 #S3 UPLOAD OBJECT
 resource "aws_s3_bucket_object" "b-error" {
   key          = "error.html"
@@ -67,4 +50,21 @@ resource "aws_s3_bucket_object" "b-index" {
   source       = "index.html"
   acl          = "public-read"
   content_type = "text/html"
+}
+
+# POLICY S3
+resource "aws_s3_bucket_policy" "b-policy" {
+  bucket = aws_s3_bucket.b.id
+
+  policy      = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::s3-95128/*",
+      }
+    ]
+	})
 }
