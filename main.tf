8442656a02dca72ac0e04dd1e4b69eac4d523049
nemar23 <66174534+nemar23@users.noreply.github.com>
2022-09-11 17:47:01 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
+# PROVIDER
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 3.0"
     }
   }
 }
@@ -12,25 +13,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
 # S3 Bucket
 resource "aws_s3_bucket" "b" {
  bucket = "aws-s3-luis-rm94405-checkpoint02"  
 }
-
-# S3 Bucket - Config ACL
-resource "aws_s3_bucket_acl" "b-acl" {
-  bucket = aws_s3_bucket.b.id
-  acl = "public-read"
-}
-
-# S3 Bucket - Config Versioning
-#resource "aws_s3_bucket_versioning" "b-versioning" {
- # bucket = aws_s3_bucket.b.id
-  #versioning_configuration {
-   # status = "enabled"
-  #}
-#}
 
 # S3 Bucket - Config Static Website
 resource "aws_s3_bucket_website_configuration" "b-website" {
@@ -43,12 +29,42 @@ resource "aws_s3_bucket_website_configuration" "b-website" {
  }
 }
 
-# S3 Bucket Objects
-resource "aws_s3_bucket_object" "b-objects" {
-    bucket = aws_s3_bucket.b.id
-    for_each = fileset("data/", "*")
-    key = each.value
-    source = "data/${each.value}"
-    acl = "public-read"
-  
+# S3 Bucket - Config ACL
+resource "aws_s3_bucket_acl" "b-acl" {
+  bucket = aws_s3_bucket.b.id
+  acl = "public-read"
+}
+
+# POLICY S3
+resource "aws_s3_bucket_policy" "b-policy" {
+  bucket = aws_s3_bucket.b.id
+
+  policy      = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::s3-95128/*",
+      }
+    ]
+	})
+}
+
+#S3 UPLOAD OBJECT
+resource "aws_s3_bucket_object" "b-error" {
+  key          = "error.html"
+  bucket       = aws_s3_bucket.b.id
+  source       = "error.html"
+  acl          = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "b-index" {
+  key          = "index.html"
+  bucket       = aws_s3_bucket.b.id
+  source       = "index.html"
+  acl          = "public-read"
+  content_type = "text/html"
 }
