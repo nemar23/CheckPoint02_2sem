1da67d44869907c2fc8cbbf1104f77e35be2c46f
nemar23 <66174534+nemar23@users.noreply.github.com>
2022-09-11 11:31:46 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,58 +1,54 @@
-# PROVIDER
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
 
-# REGION
+# Configure the AWS Provider
 provider "aws" {
-    region = "us-east-1"
-   # shared_credentials_file = ".aws/credentials"
+  region = "us-east-1"
 }
 
-# S3
-resource "aws_s3_bucket" "fiap-cloud-vds-aws-s3-alef" {
-  bucket = "aws-s3-luis-rm94405-checkpoint02"
+
+# S3 Bucket
+resource "aws_s3_bucket" "b" {
+ bucket = "aws-s3-luis-rm94405-checkpoint02"  
 }
 
-# S3 PUBLIC ACCESS
-resource "aws_s3_bucket_public_access_block" "bucket" {
-  bucket = aws_s3_bucket.aws-s3-luis-rm94405-checkpoint02.id
-
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false
-}
-
-# S3 UPLOAD OBJECT
-resource "aws_s3_bucket_object" "error" {
-  key = "error.html"
-  bucket = aws_s3_bucket.aws-s3-luis-rm94405-checkpoint02.id
-  source = "error.html"
+# S3 Bucket - Config ACL
+resource "aws_s3_bucket_acl" "b-acl" {
+  bucket = aws_s3_bucket.b.id
   acl = "public-read"
 }
 
-resource "aws_s3_bucket_object" "index" {
-  key = "index.html"
-  bucket = aws_s3_bucket.aws-s3-luis-rm94405-checkpoint02.id
-  source = "index.html"
-  acl = "public-read"
-}
+# S3 Bucket - Config Versioning
+#resource "aws_s3_bucket_versioning" "b-versioning" {
+ # bucket = aws_s3_bucket.b.id
+  #versioning_configuration {
+   # status = "enabled"
+  #}
+#}
 
-# S3 WEBSITE CONFIGURATION
-resource "aws_s3_bucket_website_configuration" "fiapwebsite" {
-  bucket = aws_s3_bucket.aws-s3-luis-rm94405-checkpoint02.id
-
+# S3 Bucket - Config Static Website
+resource "aws_s3_bucket_website_configuration" "b-website" {
+  bucket = aws_s3_bucket.b.id
   index_document {
     suffix = "index.html"
   }
+    error_document {
+      key = "error.html"
+ }
+}
 
-  error_document {
-    key = "error.html"
-  }
+# S3 Bucket Objects
+resource "aws_s3_bucket_object" "b-objects" {
+    bucket = aws_s3_bucket.b.id
+    for_each = fileset("data/", "*")
+    key = each.value
+    source = "data/${each.value}"
+    acl = "public-read"
+  
 }
